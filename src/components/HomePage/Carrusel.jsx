b06a1da76db54ecb36bb027d2f3c1c4e05837271
angel <angelriera1796@gmail.com>
2022-05-09 14:34:55 -0400

    quick view

--- src/components/HomePage/Carrusel.jsx
+++ src/components/HomePage/Carrusel.jsx
@@ -2,21 +2,44 @@ import React, { useRef } from 'react'
 
 const Carrusel = () => {
     const slider = useRef()
-    const images = [...Array(25).keys()];
+    const images = [
+        "https://static.wixstatic.com/media/ea71bb_f9b13334143b4102bc6e743068a83dd0~mv2_d_3444_4568_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_f9b13334143b4102bc6e743068a83dd0~mv2_d_3444_4568_s_4_2.webp",
+        "https://static.wixstatic.com/media/ea71bb_df1129cff14a498db09ae503498d60e6~mv2_d_2487_3298_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_df1129cff14a498db09ae503498d60e6~mv2_d_2487_3298_s_4_2.webp",
+        "https://static.wixstatic.com/media/ea71bb_4f91ddb36f2346c9870d41f83ee4c93e~mv2_d_2487_3298_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_4f91ddb36f2346c9870d41f83ee4c93e~mv2_d_2487_3298_s_4_2.webp",
+        "https://static.wixstatic.com/media/ea71bb_c9e22cba4e534026a1a7be3b15a8a648~mv2_d_2629_3487_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_c9e22cba4e534026a1a7be3b15a8a648~mv2_d_2629_3487_s_4_2.webp",
+        "https://static.wixstatic.com/media/ea71bb_f9b13334143b4102bc6e743068a83dd0~mv2_d_3444_4568_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_f9b13334143b4102bc6e743068a83dd0~mv2_d_3444_4568_s_4_2.webp",
+        "https://static.wixstatic.com/media/ea71bb_df1129cff14a498db09ae503498d60e6~mv2_d_2487_3298_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_df1129cff14a498db09ae503498d60e6~mv2_d_2487_3298_s_4_2.webp",
+        "https://static.wixstatic.com/media/ea71bb_4f91ddb36f2346c9870d41f83ee4c93e~mv2_d_2487_3298_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_4f91ddb36f2346c9870d41f83ee4c93e~mv2_d_2487_3298_s_4_2.webp",
+        "https://static.wixstatic.com/media/ea71bb_c9e22cba4e534026a1a7be3b15a8a648~mv2_d_2629_3487_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_c9e22cba4e534026a1a7be3b15a8a648~mv2_d_2629_3487_s_4_2.webp",
+        "https://static.wixstatic.com/media/ea71bb_f9b13334143b4102bc6e743068a83dd0~mv2_d_3444_4568_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_f9b13334143b4102bc6e743068a83dd0~mv2_d_3444_4568_s_4_2.webp",
+        "https://static.wixstatic.com/media/ea71bb_df1129cff14a498db09ae503498d60e6~mv2_d_2487_3298_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_df1129cff14a498db09ae503498d60e6~mv2_d_2487_3298_s_4_2.webp"
+    ];
+    const background = "https://static.wixstatic.com/media/ea71bb_df1129cff14a498db09ae503498d60e6~mv2_d_2487_3298_s_4_2.jpg/v1/fill/w_223,h_298,al_c,q_80,usm_0.66_1.00_0.01/ea71bb_df1129cff14a498db09ae503498d60e6~mv2_d_2487_3298_s_4_2.webp"
     return (
         <div className='mx-24'>
             <div className='flex items-center justify-center w-full h-full '>
-                <button className='bg-gray-500 mx-2' onClick={() => slider.current.scrollLeft -= 200}>
+                <button className='mx-2' onClick={() => slider.current.scrollLeft -= 200}>
                     <svg class='w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800' fillRule='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 19l-7-7 7-7'></path></svg>
                 </button>
                 <div ref={slider} class='snap-x overflow-x-hidden scroll-smooth h-full flex items-center justify-start'>
                     {images.map((e, i) => (
-                        <div key={i} className='snap-start flex flex-shrink-0 w-auto mx-4'>
-                            <img src={`https://picsum.photos/id/${i}/300/300`} alt={`images${i}`} className='object-cover object-center w-full' />
+                        <div key={i} className='snap-start flex flex-col flex-shrink-0 w-1/4 mx-4'>
+                            <div className='group flex flex-col items-center justify-end mb-4'  >
+                                <img src={e} alt='black chair and white table'
+                                    className='object-cover object-center w-full' />
+                                <div className='hidden group-hover:flex items-center justify-center bg-white w-full text-center -mt-14 py-4 '>
+                                    <h2 className=' text-lg'>Quick View</h2>
+                                </div>
+
+                            </div>
+                            <div className='text-center py-2 flex flex-col'>
+                                <span>im a product</span>
+                                <span className='text-red-500'>200$</span>
+                            </div>
                         </div>
                     ))}
                 </div>
-                <button className='bg-gray-500 mx-2' onClick={() => slider.current.scrollLeft += 200}>
+                <button className='mx-2' onClick={() => slider.current.scrollLeft += 200}>
                     <svg class='w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800' fillRule='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M9 5l7 7-7 7'></path></svg>
                 </button>
             </div>
--- src/routes/Navbar.jsx
+++ src/routes/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
     return (
         <div className='border-b-2 shadow-md border-gray-100 pb-5'>
 
-            <div className='max-w-7xl mx-auto my-5 px-4 sm:px-6 '>
-                <div className='flex flex-col-reverse md:flex-row min-w-full py-6 space-y-8
-        justify-center md:justify-between  md:space-x-10'>
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 '>
+                <div className='flex flex-col-reverse md:flex-row min-w-full space-y-8
+                 justify-center md:justify-between  md:space-x-10'>
 
                     {/* <div>
                         <svg className='h-[10px]' viewBox='5 5 14 14'><path fill='currentColor' d='M15.683 14.6l3.265 3.265a.2.2 0 010 .282l-.8.801a.2.2 0 01-.283 0l-3.266-3.265a5.961 5.961 0 111.084-1.084zm-4.727 1.233a4.877 4.877 0 100-9.754 4.877 4.877 0 000 9.754z'></path></svg>
@@ -99,8 +99,7 @@ const Navbar = () => {
 
             <nav id='bar' className='hidden md:flex justify-center '>
                 <div
-                    className='
-        flex flex-col md:flex-row justify-center md:space-y-0 my-4 space-y-4  text-center text-gray-500'>
+                    className='flex flex-col md:flex-row justify-center md:space-y-0 my-4 space-y-4  text-center text-gray-500'>
 
                     <a href='/' className='mx-8 hover:text-red-900'> Home </a>
                     <a href='/' className='mx-8 hover:text-red-900'> Shop All </a>
